c8357677a78b31dc129a2a809d7717fe197a6535
ridwanullah <wetwan080@gmail.com>
2025-10-08 21:32:51 +0100

    email added to shipping

--- models/order.js
+++ models/order.js
@@ -41,6 +41,7 @@ const orderSchema = new mongoose.Schema(
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
       phone: { type: String, required: true },
+      email: { type: String, required: true, unique: true },
     },
 
     paymentMethod: {
